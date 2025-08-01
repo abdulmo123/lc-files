6dbcb0ef7c46f432234de0f7e676272f4d3681ad
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2025-07-31 21:12:19 -0500

    first commit

---
+++ binary-search/33.py
@@ -0,0 +1,9 @@
+# 33. Search in Rotated Sorted Array
+# https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
