6f0e32dfe73bf64d48be23daae8be02a2132b146
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2025-08-18 19:58:16 -0500

    two sum

---
+++ two-pointers/1.py
@@ -0,0 +1,22 @@
+# Two Sum
+# https://leetcode.com/problems/two-sum
+
+"""
+### NOTES ###
+    1. two sum bruh
+""" 
+
+
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+
+        for i in range(len(nums)):
+            comp = target - nums[i]
+            if comp in hashmap:
+                return [hashmap[comp], i]
+            
+            hashmap[nums[i]] = i
+        return []
